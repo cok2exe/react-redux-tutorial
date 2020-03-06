89aa5d7b2e88cc9c6d71da4bcd2722a4430405b1
cok2exe <cok2exe@gmail.com>
2020-03-06 16:49:55 +0900

    todos container useSelector, useDispatch 적용

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,39 +1,25 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { changeInput, insert, toggle, remove } from "../modules/todos";
 import Todos from "../components/Todos";
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos
+  }));
+  const dispatch = useDispatch();
+  console.log("input", input);
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={value => dispatch(changeInput(value))}
+      onInsert={value => dispatch(insert(value))}
+      onToggle={id => dispatch(toggle(id))}
+      onRemove={id => dispatch(remove(id))}
     />
   );
 };
 
-export default connect(
-  // 비구조화 할당을 통해 tods를 분리하여,
-  // state.todos.input 대신 todos.input 사용
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove
-  }
-)(TodosContainer);
+export default TodosContainer;
